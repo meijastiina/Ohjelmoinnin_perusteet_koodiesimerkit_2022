04d8a2211f3f9f102a7a2f4b8df46b503f0b32b8
Meija Lohiniva <mlohiniva@hotmail.com>
2022-02-08 20:33:18 +0200

    Comments + getElementByID

--- 20220208/first.js
+++ 20220208/first.js
@@ -1,1 +1,10 @@
-alert("Hello!")
+/*
+
+Tämä on Ohjelmoinnin perusteiden ensimmäinen harjoitus JS-tiedosto
+
+
+*/
+
+// Tässä haetaan HTML-elementti, jonka id-attribuutin arvo on otsikko ja asetetaan sen sisällöksi Hello again
+
+document.getElementById("otsikko").innerHTML = "Hello again!";
