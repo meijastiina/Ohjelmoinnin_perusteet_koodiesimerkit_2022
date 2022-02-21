ded28a6b0829735022f4048a91fa17615b8f63df
Meija Lohiniva <mlohiniva@hotmail.com>
2022-02-21 14:17:32 +0200

    Päivitetty functions

--- 20220221/functions.js
+++ 20220221/functions.js
@@ -1,34 +1,40 @@
-/**
- * MATH
- * 
- * 
- *  */ 
+// Haetaan tulostuselementti DOM:sta muuttujaan, jotta DOM-hakuja ei tarvitse tehdä useampia
+let resultElement = document.getElementById("resultElement");
+// Käsitellään painikkeen klikkaus
+document.getElementById("testButton").onclick = function() {
+    /**
+     * MATH
+     * 
+     * 
+     *  */ 
 
-// PI
-
-// round(), ceil(), floor()
-
-// min(), max()
-
-// random()
-
-// pow()
-
-// sqrt()
+    // PI
 
 
-/**
- * STRING
- */
+    // round(), ceil(), floor()
 
-// length
+    // min(), max()
 
-// slice(), substr()
+    // random()
 
-// replace()
+    // pow()
 
-// toUpperCase(), toLowerCase()
+    // sqrt()
 
-// trim()
 
-// charAt()
+    /**
+     * STRING
+     */
+
+    // length
+
+    // slice(), substr()
+
+    // replace()
+
+    // toUpperCase(), toLowerCase()
+
+    // trim()
+
+    // charAt()
+}
